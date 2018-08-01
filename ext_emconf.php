515576147389e3e3f71a18d52aaeb6c14593e8eb
Christian Plattner <ccpp@gmx.at>
2018-08-01 12:42:08 +0200

    Convert project to TYPO3 extension

---
+++ ext_emconf.php
@@ -0,0 +1,34 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Dinnerclub Innsbruck Theme',
+	'description' => '',
+	'category' => 'frontend',
+	'author' => 'Plattner Christian, scre',
+	'author_email' => 'ccpp@gmx.at',
+	'author_company' => '',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.1',
+	'constraints' => array(
+		'depends' => array(
+			'extbase' => '6.0',
+			'fluid' => '6.0',
+			'typo3' => '6.0',
+			'dinnerclub' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
