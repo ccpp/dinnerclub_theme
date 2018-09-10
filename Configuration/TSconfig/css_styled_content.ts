6b9f3db6f97ec8f74328c9de00a0ba887f32925d
Christian Plattner <ccpp@gmx.at>
2018-09-11 00:20:04 +0200

    Add image layout (aka imageorient) for parallax scrolling

---
+++ Configuration/TSconfig/css_styled_content.ts
@@ -0,0 +1,10 @@
+TCEFORM.tt_content {
+    imageorient.types {
+        image.addItems {
+            88 = Parallax Scroll
+        }
+        textpic.addItems {
+            88 = Parallax Scroll
+        }
+    }
+}
--- ext_tables.php
+++ ext_tables.php
@@ -1,3 +1,7 @@
 <?php
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'TypoScript', 'Dinnerclub Theme');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+<INCLUDE_TYPOSCRIPT: source="FiLE:EXT:dinnerclub_theme/Configuration/TSconfig/css_styled_content.ts">
+');
