3ae2ee18432990f5ee26dcd4177c5bc64d4b7138
Christian Plattner <ccpp@gmx.at>
2018-12-01 13:12:43 +0100

    Add shadow frame option to section_frame

--- Configuration/TSconfig/css_styled_content.ts
+++ Configuration/TSconfig/css_styled_content.ts
@@ -7,4 +7,17 @@ TCEFORM.tt_content {
             88 = Parallax Scroll
         }
     }
+
+    layout {
+        removeItems = 1,2,3
+    }
+
+    section_frame {
+        removeItems = 5,6,11,12
+        altLabels {
+            10 = Schmal
+            20 = Box mit Schatten
+            21 = Schmal, mit Schatten
+        }
+    }
 }
